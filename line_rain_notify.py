5b9d33cb087813b3985145ddd70c0ec2c8b2d5aa
KIMATA Tetsuya <kimata@green-rabbit.net>
2019-09-22 18:33:25 +0900

    feat: ログを残すようにした

--- line_rain_notify.py
+++ line_rain_notify.py
@@ -3,13 +3,22 @@
 
 from influxdb import InfluxDBClient
 import requests
+import time
 from datetime import datetime
 from config import LINE_NOTIFY_TOKEN
 from pathlib import Path
 import os
 import urllib.request
 
+import traceback
+
+import logging
+import logging.handlers
+import gzip
+
 RADAR_MAP_URL_BASE = 'http://www.jma.go.jp/jp/radnowc/imgs/radar/211/'
+RADAR_MAP_URL_BASE2 = 'https://static.tenki.jp/static-images/radar/%Y/%m/%d/%H/{:02d}/00/pref-26-large.jpg'
+
 LINE_NOTIFY_API_ENDPOINT = 'https://notify-api.line.me/api/notify'
 
 INFLUXDB_ADDR = '192.168.2.20'
@@ -22,16 +31,23 @@ INFLUXDB_QUERY = """
 SELECT MEAN("touchpad") FROM "sensor.esp32" WHERE ("hostname" = \'ESP32-raindrop\') AND time >= now() - 1h GROUP BY time(2m) FILL(previous) ORDER by time asc
 """
 
-WET_THRESHOLD = 370
+WET_ON_THRESHOLD = 370
+WET_OFF_THRESHOLD = 380
 
 NOTIFY_FLAG_FILE = '.notify'
 NOTIFY_INTERVAL = 7200
 
 def radar_map_url():
-    return RADAR_MAP_URL_BASE + \
-        datetime.today().strftime('%Y%m%d%H') \
-        + '{:02d}'.format(datetime.today().minute // 5 * 5) \
-        + '-00.png'
+#     return RADAR_MAP_URL_BASE + \
+#         datetime.today().strftime('%Y%m%d%H') \
+#         + '{:02d}'.format(datetime.today().minute // 5 * 5) \
+#         + '-00.png'
+
+    url = datetime.today().strftime(RADAR_MAP_URL_BASE2).format(datetime.today().minute // 5 * 5)
+    logger.info('radar_map_url: {}'.format(url))
+
+    return url
+
 
 def notify_flag_file():
     return os.path.join(os.path.dirname(__file__), NOTIFY_FLAG_FILE)
@@ -64,9 +80,9 @@ def check_soil_wet():
     for val in status_list:
         if (val is None): # NOTE: データが登録されていない期間は None
             continue
-        elif (val < WET_THRESHOLD):
+        elif (val < WET_ON_THRESHOLD):
             thresh_below += 1
-        elif (val > WET_THRESHOLD):
+        elif (val > WET_OFF_THRESHOLD):
             thresh_above += 1
             thresh_below = 0 # NOTE: reset count
 
@@ -83,6 +99,37 @@ def check_already_notified():
     return elapsed_time > NOTIFY_INTERVAL
 
 
-if check_soil_wet() and check_already_notified():
-    line_notify('\U00002614' + '️雨が降り始めました！',
-                urllib.request.urlopen(radar_map_url()))
+class GZipRotator:
+    def __call__(self, source, dest):
+        os.rename(source, dest)
+        f_in = open(dest, 'rb')
+        f_out = gzip.open("%s.gz" % dest, 'wb')
+        f_out.writelines(f_in)
+        f_out.close()
+        f_in.close()
+        os.remove(dest)
+
+
+
+logger = logging.getLogger()
+log_handler = logging.handlers.RotatingFileHandler(
+    'line_rain_notify.log',
+    encoding='utf8', maxBytes=10*1024*1024, backupCount=10,
+)
+log_handler.formatter = logging.Formatter(
+    fmt='%(asctime)s %(levelname)s %(name)s :%(message)s',
+    datefmt='%Y/%m/%d %H:%M:%S %Z'
+)
+log_handler.formatter.converter = time.gmtime
+log_handler.rotator = GZipRotator()
+
+logger.addHandler(log_handler)
+logger.setLevel(level=logging.INFO)
+
+
+try:
+    if check_soil_wet() and check_already_notified():
+        line_notify('\U00002614' + '️雨が降り始めました！',
+                    urllib.request.urlopen(radar_map_url()))
+except:
+    logger.error(traceback.format_exc())
